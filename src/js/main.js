33bcd5dc2c58ba43a166b75087183e02f931bdf7
Derrick Showers <derrick@derrickshowers.com>
2014-06-07 00:05:27 -0700

    adding appendTime function which takes the routeType, routeDirection and nextTime and appends to the DOM. also added getTimes, which makes a GET request to the API to get the departure information

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,47 @@
 (function($) {
   'use strict';
 
-  // defaults
-  var apiKey = '5e58e873-398c-4408-87f4-d58b19136466';
-  var URL = 'http://services.my511.org/Transit2.0/';
+  var settings = {
+    resultsArea: document.getElementById('results')
+  }
 
-  // first request
-  var requestURL = URL + '/GetAgencies.aspx?' + apiKey;
-  $.get(requestURL, function(data) {
-    console.log(data);
-  })
+  function appendTime(routeType, routeDirection, nextTime) {
+    var li,
+        textNode,
+        ul = document.createElement('ul');
+
+    for (var i = 0; i < arguments.length; i++) {
+      li = document.createElement('li');
+      textNode = document.createTextNode(arguments[i]);
+      li.appendChild(textNode);
+      ul.appendChild(li);
+    }
+
+    settings.resultsArea.appendChild(ul);
+  }
+
+  function getTimes(stopCode) {
+    $.get('/api/times/' + stopCode, function(data) {
+      var routeType,
+          routeDirection,
+          nextTime,
+          cleanData = data.RTT.AgencyList[0].Agency[0].RouteList[0].Route;
+
+      for (var i = 0; i < cleanData.length; i++) {
+        routeType = cleanData[i].$.Name;
+        for (var i2 = 0; i2 < cleanData[i].RouteDirectionList[0].RouteDirection.length; i2++) {
+          routeDirection = cleanData[i].RouteDirectionList[0].RouteDirection[i2].$.Name;
+          nextTime = cleanData[i].RouteDirectionList[0].RouteDirection[i2].StopList[0].Stop[0].DepartureTimeList[0].DepartureTime;
+          if (nextTime !== undefined) {
+            nextTime = nextTime[0];
+            appendTime(routeType, routeDirection, nextTime);
+          }
+        }
+      }
+    });
+  }
+
+  // init
+  getTimes('70012');
 
 })(jQuery);
