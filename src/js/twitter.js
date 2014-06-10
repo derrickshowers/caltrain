ac3104fedac05b55046dc1fb206cd3ae4bf5e047
Derrick Showers <derrick@derrickshowers.com>
2014-06-09 23:04:11 -0700

    twitter animation from bottom

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,19 +1,41 @@
 (function($) {
   'use strict';
 
-  var $twitterContainer = $('.twitter-timeline');
-  var $twitterHeading = $('#twitter-heading');
+  var $twitterHeading = $('#twitter-heading'),
+      deviceHeight = $(window).height(),
+      headingHeight = $twitterHeading.height();
 
-  /**
-   * does just what you'd think it does
-   * no return, no parmas, just init
-   */
-  function init() {
+  // click handler
+  $twitterHeading.on('click', function(e) {
+    if ($twitterHeading.offset().top === 0) {
+      $(this).animate({
+        bottom: 0,
+        'background-color': '#DC3C41',
+      }, 300, function() {
+        $(this).text('Return to Twitter');
+        $(this).animate({
+          color: '#FFF'
+        }, 1000);
+      });
+      $('.twitter-timeline-rendered').animate({
+        height: 0
+      }, 300);
+    } else {
+      $(this).animate({
+        bottom: deviceHeight - headingHeight,
+        'background-color': '#FFF',
+      }, 300, function() {
+        $(this).text('Touch to return');
+        $(this).animate({
+          color: '#DC3C41'
+        }, 1000);
+      });
+      $('.twitter-timeline-rendered').show();
+      $('.twitter-timeline-rendered').animate({
+        height: deviceHeight - headingHeight
+      }, 300);
+    }
 
-    var deviceHeight = $(window).height();
-
-  }
-
-  init();
+  });
 
 })(jQuery);
