0eaea0f40ff6ac303a6409f852275a9a396b5388
Derrick Showers <derrick@derrickshowers.com>
2014-06-09 22:11:32 -0700

    adding js file for interactive twitter element

---
+++ src/js/twitter.js
@@ -0,0 +1,19 @@
+(function($) {
+  'use strict';
+
+  var $twitterContainer = $('.twitter-timeline');
+  var $twitterHeading = $('#twitter-heading');
+
+  /**
+   * does just what you'd think it does
+   * no return, no parmas, just init
+   */
+  function init() {
+
+    var deviceHeight = $(window).height();
+
+  }
+
+  init();
+
+})(jQuery);
